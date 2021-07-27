265f24da41fcdb39c40d91ef200384594d58bc96
Eric Neidhardt <eric.neidhardt@gmail.com>
2021-07-27 17:00:02 +0200

    Configure log output

--- cmd/goclean/main.go
+++ cmd/goclean/main.go
@@ -7,22 +7,23 @@ import (
 	"os"
 
 	cleanup "github.com/EricNeid/go-clean"
+	lumberjack "gopkg.in/natefinch/lumberjack.v2"
 )
 
 var (
 	dir     string
 	maxAgeD int
+	logFile string
 )
 
 func init() {
-	// TODO syslog
-
 	log.SetFlags(0)
 	log.SetPrefix("goclean: ")
-	log.SetOutput(io.MultiWriter(os.Stdout))
+	log.SetOutput(os.Stdout)
 
 	flag.StringVar(&dir, "d", dir, "set directory to cleanup")
 	flag.IntVar(&maxAgeD, "t", maxAgeD, "clean all files that are older than 't' days")
+	flag.StringVar(&logFile, "l", logFile, "Optional: write log to given file")
 	flag.Parse()
 }
 
@@ -37,6 +38,20 @@ func main() {
 		log.Println("maximum age for cleanup must be larger than 0")
 		flag.Usage()
 		os.Exit(1)
+	}
+
+	if len(logFile) >= 0 {
+		log.SetOutput(
+			io.MultiWriter(
+				os.Stdout,
+				&lumberjack.Logger{
+					Filename:   logFile,
+					MaxSize:    500, // megabytes
+					MaxBackups: 3,
+					MaxAge:     28, //days
+				},
+			),
+		)
 	}
 
 	expiredFiles := cleanup.GetExpiredFiles(dir, maxAgeD*24)
